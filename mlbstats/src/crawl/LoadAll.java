052ad169156d202b8b14c54baa9b824c982ee633
Mike <migross76@users.noreply.github.com>
2015-10-26 11:05:25 -0400

    uncommented driver

--- mlbstats/src/crawl/LoadAll.java
+++ mlbstats/src/crawl/LoadAll.java
@@ -28,19 +28,19 @@ public class LoadAll {
   public static void main(String[] args) throws Exception {
     try (MyDatabase db = new MyDatabase()) {
       // load SQL schema(s)
-      //LoadSchema.load(new File("C:/build/mlbstats/sql/tables-lahman.sql"));
-      //LoadSchema.load(new File("C:/build/mlbstats/sql/tables-bbref.sql"));
+      LoadSchema.load(new File("C:/build/mlbstats/sql/tables-lahman.sql"));
+      LoadSchema.load(new File("C:/build/mlbstats/sql/tables-bbref.sql"));
       // load Lahman CSV files
-      //ImportCSV.load(db, new File(LAHMAN_PATH));
+      ImportCSV.load(db, new File(LAHMAN_PATH));
       // crawl BBRef WAR files
-      //BBRefCSV C = new BBRefCSV(BBRefCSV.CACHE);
-      //C.fetch(db, "bat", "rbWar", BBRefCSV.BAT_FIELDS);
-      //C.fetch(db, "pitch", "rpWar", BBRefCSV.PITCH_FIELDS);
+      BBRefCSV C = new BBRefCSV(BBRefCSV.CACHE);
+      C.fetch(db, "bat", "rbWar", BBRefCSV.BAT_FIELDS);
+      C.fetch(db, "pitch", "rpWar", BBRefCSV.PITCH_FIELDS);
       // crawl ELO files
-      //ELO elo = new ELO("C:/build/mlbstats/cache/elo");
-      //Date D = new Date();
-      //elo.ingest(db, D, ELO.CODE_BATTER);
-      //elo.ingest(db, D, ELO.CODE_PITCHER);
+      ELO elo = new ELO("C:/build/mlbstats/cache/elo");
+      Date D = new Date();
+      elo.ingest(db, D, ELO.CODE_BATTER);
+      elo.ingest(db, D, ELO.CODE_PITCHER);
       // crawl schedule files
       new Schedule(db, BBRefCSV.CACHE).execute(db);
     }
